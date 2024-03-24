4b2af639f2af9be6fa974b0ad8b75c9cf31424f6
fsmosca <fsmosca@users.noreply.github.com>
2024-03-24 17:55:57 +0800

    Add content

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,19 @@ st.set_page_config(page_title='Image generator', layout='centered')
 def main():
     Navigator()
 
-    st.title('Welcome to image generator :fire:')
+    st.title('Welcome!!')
+
+    st.markdown('''
+        A web app built using [streamlit](https://streamlit.io/) that can generate images using Openai's [DALL.E](https://platform.openai.com/docs/guides/images?context=node). This is not free, you have to
+        supply an api key from Openai.
+                
+        Images from DALL.E.3 model starts at size 1024x1024. Whereas that of DALL.E.2 can generate for as small as 256x256. You can preview 
+        and download the generated images.
+                
+        The model DALL.E.2 can generate more than 1 image at once. DALL.E.3 can only generate a single image at a time.
+
+        '''
+    )
 
 
 if __name__ == '__main__':
