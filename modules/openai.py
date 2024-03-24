b14ee5bc6cb3bcae381819ce6c1f0c32a99b7bc5
fsmosca <fsmosca@users.noreply.github.com>
2024-03-24 16:36:09 +0800

    Add more parameters to generate dall-e images
    * If more than 1 images are generated, create multiple image previews and provide a download buttons.

--- modules/openai.py
+++ modules/openai.py
@@ -1,9 +1,20 @@
+from streamlit import session_state as ss
 import openai
 
 
-def generate_image(apikey, model, size, prompt):
+MODELS = ['dall-e-3', 'dall-e-2']
+
+
+def generate_image(apikey, model, size:str, prompt, num_images:int, style:str, quality:str):
     """Generates image based on prompt and DALL.E."""
-    error_message, image_b64_json = None, None
+    if 'save' not in ss:
+        ss.save = []
+    else:
+        ss.save = []
+
+    num_images = 1 if model == MODELS[0] else num_images
+
+    error_message = None
 
     client = openai.OpenAI(api_key=apikey)
 
@@ -12,8 +23,9 @@ def generate_image(apikey, model, size, prompt):
             model=model,
             prompt=prompt,
             size=size,
-            quality="standard",
-            n=1,
+            n=num_images,
+            style=style,
+            quality=quality,
             response_format="b64_json",
         )
     except Exception as err:
@@ -21,5 +33,6 @@ def generate_image(apikey, model, size, prompt):
     else:
         for image in response.data:
             image_b64_json = image.b64_json
+            ss.save.append(image_b64_json)
 
-    return image_b64_json, error_message
+    return error_message
