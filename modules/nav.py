009030de2f246e77e50e8eeb7c43b1cb50acdb19
fsmosca <fsmosca@users.noreply.github.com>
2024-03-24 20:50:13 +0800

    Update nav.py

--- modules/nav.py
+++ modules/nav.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 
 def HomeNav():
-    st.sidebar.page_link('streamlit_app.py', label='Home')
+    st.sidebar.page_link('streamlit_app.py', label='Home', icon='🌳')
 
 
 def DalleNav():
-    st.sidebar.page_link('./pages/dalle.py', label='Image Generator')
+    st.sidebar.page_link('./pages/dalle.py', label='Image Generator', icon='🌿')
 
 
 def Navigator():
